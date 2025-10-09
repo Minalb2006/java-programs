4f2f31e24494804405ede838ba2d29ba5817c224
Minalb2006 <minalborkar0012@gmail.com>
2025-10-09 20:38:47 +0530

    first commit

---
+++ conditions.js
@@ -0,0 +1,116 @@
+
+//If statement
+var a=10;
+var b=5;
+if(a>b)
+{
+    console.log("a is greater than b");
+}
+
+//Else statement
+var m=10;
+var c=5;
+if(m>c)
+{
+    console.log("m is greater");
+}
+else
+{
+    console.log("c is greater");
+}
+
+//Else statement 2
+var p=50;
+var q=50;
+if(p>=q)
+{
+    console.log("p is queen");
+}
+else{
+    console.log("q is king");
+}
+
+//Else if statement
+var minal=12;
+if(minal>=18)
+{
+    console.log("eigible for voting");
+}
+else if(minal>=16)
+{
+    console.log("not eigible for voting");
+}
+else
+{
+    console.log("not eigible for voting");
+}
+
+//Else if statement HOMEWORK TODAY
+var age=12;
+
+if(age==10)
+{
+    console.log("payment has been credited by 1000 rupees only");
+}
+else if(age==11)
+{
+    console.log("payment credited 2000 rupees only");
+}
+else if(age==12)
+{
+    console.log("payment has been credited by 3000 rupees only");
+}
+else if(age==13)
+{
+    console.log("payment has been credited by 4000 rupees only");
+}
+else if(age==14)
+{
+    console.log("payment has been credited by 5000 rupees only");
+}
+else if(age==15)
+{
+    console.log("payment has been credited by 6000 rupees only");
+}
+else{
+    console.log("no payment has been credited");
+}
+
+//Else if statement HOMEWORK TODAY 2
+var button=2;
+
+if(button==1)
+{
+    console.log("turn on fan");
+}
+else if(button==2)
+{
+    console.log("turn on T.V");
+}
+else if(button==3)
+{
+    console.log("turn on lights");
+}
+else if(button==4)
+{
+    console.log("turn on AC");
+}
+else if(button==5)
+{
+    console.log("turn on geyser");
+}
+else if(button==6)
+{
+    console.log("turn on gas");
+}
+else if(button==7)
+{
+    console.log("turn on fridge");
+}
+else if(button==8)
+{
+    console.log("turn off all appliances at same time");
+}
+else{
+    console.log("no appliance is on");
+}
---
+++ operators.js
@@ -0,0 +1,55 @@
+console.log("Minal here!")
+
+//  addition
+ var a=20;
+var b=30;
+console.log(a+b);
+
+// substraction
+var a=30;
+var b=70;
+console.log(a-b);
+
+// Division
+var a=40;
+var b=20;
+console.log(a/b);
+
+// multiplication
+var a=3;
+var b=7;
+console.log(a*b);
+
+// modulas
+var a=10;
+var b=3;
+console.log(a%b);
+
+// exponention
+var a=2;
+var b=3;
+console.log(a**b);
+
+// increment
+var a=6;
+var b=++a;
+console.log(b);
+
+// decrement
+var a=6;
+var b=--a;
+console.log(b);
+
+//equalto equaleto
+var a=7;
+var b=7;
+{
+    console.log(a==b);
+}
+
+//equalto equalto equalto
+var a=100;
+var b=101;
+{
+    console.log(a===b);
+}
