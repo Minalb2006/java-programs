4e2bebef945609bf4e3be17abf7ca16c136fac66
Minalb2006 <minalborkar0012@gmail.com>
2025-10-10 13:15:45 +0530

    gates and loop adding

--- gates.js
+++ gates.js
@@ -76,3 +76,47 @@ if(!(a>b))
 {
     console.log("false");
 }
+
+//Switch Case Example
+var days=4;
+switch(days)
+ {
+case 1:
+{
+    console.log("Monday");
+}
+break;
+case 2:
+{
+    console.log("Tuesday");
+}
+break;
+case 3:
+{
+    console.log("Wednesday");
+}
+case 4:
+    {
+        console.log("Thrusday");
+    }
+    break;
+case 5:
+    {
+        console.log("Friday");
+    }
+    break;
+case 6:
+    {
+        console.log("Saturday");
+    }
+    break;
+case 7:
+    {
+         console.log("Sunday");
+    }
+    break;
+default:
+    {
+        console.log("Invalid Input");
+    }
+}
