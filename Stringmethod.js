5b2da599c4a2d9b3a0e12671ed06b1c0d3a5db87
Minalb2006 <minalborkar0012@gmail.com>
2025-10-13 15:02:32 +0530

    done

---
+++ Stringmethod.js
@@ -0,0 +1,66 @@
+//do while break//
+const p = 5;
+let n = 1;
+do {
+    if(n===5){
+    break;
+
+    }
+    console.log (p * n);
+    n++;
+} while (n <= 10);
+
+//do while continue//
+const s = 5;
+let b = 1;
+do {
+    if(b==5){
+    continue;
+
+    }
+    console.log (s * b);
+    n++;
+} while (b <= 10);
+
+//.length//
+var string = "Minal";
+console.log(string.length);
+
+//uppercase//
+console.log(string.toUpperCase());
+
+//lowercase//
+console.log(string.toLowerCase());
+
+//indexof//
+console.log("minal world".indexOf("o"));
+
+//lastindexof//
+console.log("minal world".lastIndexOf("w"));
+
+//replace one word to another//
+console.log("minal world".replace("world", "there"));
+
+//repeat words or letters//
+console.log("minal".repeat(3));
+
+//charAt 4 index vr konta 
+console.log("minal".charAt(4));
+
+//promt
+var name=prompt("Enter your name");
+console.log("your name is : ",name)
+
+//alert
+let alt=alert("Today is Saturday")
+
+
+
+
+
+
+
+
+
+
+ 
